f02ad4ba2dfa053fa901502440900e4be605a13c
Daniel Drake <dsd@laptop.org>
2009-12-22 19:59:56 +0000

    Fix jffs2 module to create bootable layout

--- modules/jffs2_image/image.50.makeimg.sh
+++ modules/jffs2_image/image.50.makeimg.sh
@@ -4,10 +4,30 @@
 . $OOB__shlib
 
 buildnr=$(read_buildnr)
+versioned_fs=$(read_config base versioned_fs)
+
+tmpdir=$intermediatesdir/jffs2-fs
 tmpimg=$intermediatesdir/jffs2tmp.img
 img=$outputdir/os$buildnr.img
 
-mkfs.jffs2 -x rtime -n -e128KiB -r $fsmount -o $tmpimg
+
+echo "Copying image contents..."
+rm -rf $tmpdir
+cp -a $fsmount $tmpdir
+
+if [ "$versioned_fs" = "1" ]; then
+	# setup for unpartitioned boot of versioned fs
+	mkdir -p $tmpdir/versions/configs/$buildnr
+	ln -s versions/boot/current/boot $tmpdir/boot
+	ln -s versions/boot/alt/boot $tmpdir/boot-alt
+	ln -s configs/$buildnr $tmpdir/versions/boot
+	ln -s /versions/pristine/$buildnr $tmpdir/versions/configs/$buildnr/current
+fi
+
+echo "Making JFFS2 image..."
+mkfs.jffs2 -x rtime -n -e128KiB -r $tmpdir -o $tmpimg
+
+echo "Creating checksums..."
 sumtool -n -p -e 128KiB -i $tmpimg -o $img
 $bindir/crcimg $img
 
