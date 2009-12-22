69329e988ff33f4bd5456510e8da262d6e5d875a
Daniel Drake <dsd@laptop.org>
2009-12-22 10:07:59 +0000

    Cleanup image-creator mounts

--- modules/base/cleanup.50.cleanup.sh
+++ modules/base/cleanup.50.cleanup.sh
@@ -8,3 +8,21 @@ umount $intermediatesdir/mnt-boot &>/dev/null || :
 umount $intermediatesdir/mnt-squashfs &>/dev/null || :
 umount $intermediatesdir/mnt-iso &>/dev/null || :
 
+# Sometimes, when image-creator is interrupted, it leaves mounts around.
+# clean them up here
+
+# instead of trying to figure out which order to umount the appropriate
+# mounts, just take a brute-force approach
+for tmp in $(seq 10); do
+	oIFS=$IFS
+	IFS=$'\n'
+	for i in $(</proc/mounts); do
+		i=${i#* }
+		i=${i%% *}
+		if [[ "${i:0:18}" == "/var/tmp/imgcreate" ]]; then
+			umount $i &>/dev/null || :
+		fi
+	done
+	IFS=oIFS
+done
+
