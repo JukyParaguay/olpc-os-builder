54443c0d904839e009d32d27b8bf42d901980561
Daniel Drake <dsd@laptop.org>
2010-04-12 15:20:44 -0300

    Add module for configuring OATS server
    Used by olpc-update-query

---
+++ modules/oats_cfg/kspost.50.oats_cfg.sh
@@ -0,0 +1,16 @@
+# Copyright (C) 2010 One Laptop Per Child
+# Licensed under the terms of the GNU GPL v2 or later; see COPYING for details.
+
+. $OOB__shlib
+
+server=$(read_config oats_cfg server)
+ignore_xs=$(read_config oats_cfg ignore_xs)
+
+if [ "$ignore_xs" = "1" ]; then
+	echo "touch /security/oats-ignore-xs"
+fi
+
+if [ -n "$server" ]; then
+	echo "echo '$server' > /security/oats-server"
+fi
+
