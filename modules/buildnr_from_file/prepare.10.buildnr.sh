287bf28761b6eced38aa09365f73e7e1fe1a0e58
Daniel Drake <dsd@laptop.org>
2010-06-29 17:03:32 -0500

    buildnr_from_file: fix compatibility with newer bash
    The return value semantics of the arithmetic operator seems to have
    changed, returning a code which is being interpreted as an error.

--- modules/buildnr_from_file/prepare.10.buildnr.sh
+++ modules/buildnr_from_file/prepare.10.buildnr.sh
@@ -7,7 +7,9 @@ nr=0
 path=$(read_config buildnr_from_file path)
 [ -n "$path" -a -e "$path" ] && nr=$(<$path)
 
-(( nr++ ))
+# increment and ignore return value. new versions of bash return the new
+# value of the variable which is usually non-zero.
+(( nr++ )) || :
 
 echo $nr > $intermediatesdir/buildnr
 
