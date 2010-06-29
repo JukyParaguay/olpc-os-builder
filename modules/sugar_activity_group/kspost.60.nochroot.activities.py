JukyParaguay/olpc-os-builder

commit fb74bbaf81fe2d095fbadefaea218fdb740e93f2

sugar_activity_group: configure activity group as systemwide by default
Otherwise you can't change the activity group in an incremental OS
update, which is potentially asking for trouble in the field.

modules/sugar_activity_group/kspost.60.nochroot.activities.py
@@ -172,6 +172,8 @@ if not os.path.exists(cache):
 baseurl = ooblib.read_config('sugar_activity_group', 'url')
 install_activities = ooblib.read_config_bool('sugar_activity_group',
                                              'install_activities')
+systemwide = ooblib.read_config_bool('sugar_activity_group',
+                                     'activity_group_systemwide')
 
 if install_activities:
     vmaj = int(ooblib.read_config('global', 'olpc_version_major'))
@@ -226,6 +228,10 @@ if install_activities:
         # only process the first working URL
         break
 
-print "echo '%s' > $INSTALL_ROOT/home/olpc/Activities/.groups" % baseurl
-print "chown -R 500:500 $INSTALL_ROOT/home/olpc/{Activities,Library}"
+if systemwide:
+	print "mkdir -p $INSTALL_ROOT/etc/olpc-update"
+	print "echo '%s' > $INSTALL_ROOT/etc/olpc-update/activity-groups" % baseurl
+else:
+	print "echo '%s' > $INSTALL_ROOT/home/olpc/Activities/.groups" % baseurl
+	print "chown -R 500:500 $INSTALL_ROOT/home/olpc/{Activities,Library}"
 

modules/sugar_activity_group/README
@@ -20,3 +20,13 @@ installed. e.g.:
 url=http://wiki.laptop.org/go/Activities/G1G1
 install_activities=0
 
+
+By default, the activity group will be written to the system-wide configuration file /etc/olpc-update/activity-groups. The activity_group_systemwide option
+can be disabled in order to use the /home/olpc/Activities/.groups location
+instead (note that this file resides outside of the OS, so cannot be updated
+with an OS update). e.g.:
+
+[sugar_activity_group]
+url=http://wiki.laptop.org/go/Activities/G1G1
+activity_group_systemwide=0
+

modules/sugar_activity_group/defaults.ini
@@ -1,3 +1,4 @@
 [sugar_activity_group]
 install_activities=1
+activity_group_systemwide=1
 

commit 03595e55b2ee68cd7ea45a2b208b5617dd0e3a9b

sugar_activity_group: fix activity ownership
Commit fb74bbaf81fe introduced a bug where activity ownership was only
set to OLPC on configurations where a non-systemwide activity group
URL was installed.

modules/sugar_activity_group/kspost.60.nochroot.activities.py
@@ -233,5 +233,6 @@ if systemwide:
 	print "echo '%s' > $INSTALL_ROOT/etc/olpc-update/activity-groups" % baseurl
 else:
 	print "echo '%s' > $INSTALL_ROOT/home/olpc/Activities/.groups" % baseurl
-	print "chown -R 500:500 $INSTALL_ROOT/home/olpc/{Activities,Library}"
+
+print "chown -R 500:500 $INSTALL_ROOT/home/olpc/{Activities,Library}"
 