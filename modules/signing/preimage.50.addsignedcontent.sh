9f20418922d04bc6fe30b2aa1fb5daaabddf4101
Daniel Drake <dsd@laptop.org>
2012-09-10 14:22:55 -0600

    Always ship kernel/initramfs in zip files
    We now ship the kernel/initramfs in zip files only.
    This reduces duplication for when images get signed, and it allows
    us to ship a simplified olpc.fth.
    
    This implements the plan here:
    http://lists.laptop.org/pipermail/devel/2012-March/034439.html
    
    This didn't work on first attempt because we hit a firmware bug
    on XO-1.75 (#12107). Now that we have a fix, and a workaround for
    old firmware versions, give this change another spin.

--- modules/signing/preimage.50.addsignedcontent.sh
+++ modules/signing/preimage.50.addsignedcontent.sh
@@ -12,14 +12,7 @@ rm -rf $signdir
 mkdir -p $signdir
 unzip $content -d $signdir
 for sfile in bootfw.zip runos.zip runrd.zip actos.zip actrd.zip; do
-	[ -e $signdir/$sfile ] && cp $signdir/$sfile $fsmount/boot/$sfile
+	[ -e $signdir/$sfile ] && cp --remove-destination $signdir/$sfile $fsmount/boot/$sfile
 done
 
 rm -rf $signdir
-
-# symlink actXX to runXX (or the other way) if any of them are missing
-[ -e $fsmount/boot/actos.zip ] || ln -s runos.zip $fsmount/boot/actos.zip
-[ -e $fsmount/boot/actrd.zip ] || ln -s runrd.zip $fsmount/boot/actrd.zip
-[ -e $fsmount/boot/runos.zip ] || ln -s actos.zip $fsmount/boot/runos.zip
-[ -e $fsmount/boot/runrd.zip ] || ln -s actrd.zip $fsmount/boot/runrd.zip
-
