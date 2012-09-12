8127b8be13956e98ad7bade38b207959911328cc
Daniel Drake <dsd@laptop.org>
2012-09-12 14:00:53 -0600

    oats_cfg: add option to ignore signatures

--- modules/oats_cfg/kspost.50.oats_cfg.sh
+++ modules/oats_cfg/kspost.50.oats_cfg.sh
@@ -5,11 +5,14 @@
 
 server=$(read_config oats_cfg server)
 ignore_xs=$(read_config oats_cfg ignore_xs)
+ignore_sig=$(read_config oats_cfg ignore_signature)
 stream=$(read_config oats_cfg stream)
 
 if [ "$ignore_xs" = "1" ]; then
 	echo "touch /etc/oats-ignore-xs"
 fi
+
+[ "$ignore_sig" = "1" ] && echo "touch /etc/oats-ignore-signature"
 
 if [ -n "$server" ]; then
 	echo "echo '$server' > /etc/oats-server"
@@ -18,4 +21,3 @@ fi
 if [ -n "$stream" ]; then
 	echo "echo '$stream' > /etc/update-stream"
 fi
-
