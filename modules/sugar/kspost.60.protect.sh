7e7e90f4967913e797fba813ea3dbac3ccbf5106
Martin Abente Lahaye <tch@sugarlabs.org>
2014-05-13 09:13:43 -0400

    Port gconf settings to gsettings
    Signed-off-by: Martin Abente Lahaye <tch@sugarlabs.org>

--- modules/sugar/kspost.60.protect.sh
+++ modules/sugar/kspost.60.protect.sh
@@ -10,11 +10,18 @@ if [[ -n "$activities" ]]; then
     IFS=$'\n\t, '
     for activity in $activities; do
         if [[ -n "$list" ]]; then
-            list=$list','$activity
+            list=$list,"'${activity}'"
         else
-            list=$activity
+            list="'${activity}'"
         fi
     done
-    echo "gconftool-2  --direct --config-source xml:readwrite:/etc/gconf/gconf.xml.defaults --type list --list-type string --set /desktop/sugar/protected_activities [$list]"
+
+echo " 
+cat >/usr/share/glib-2.0/schemas/sugar.oob.protected.gschema.override <<EOF
+[org.sugarlabs]
+protected-activities=[${list}]
+EOF
+/usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas"
+
     IFS=$oIFS
 fi
